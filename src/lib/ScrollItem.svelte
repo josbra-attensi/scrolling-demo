c86f785cd1f7dfb5b38e20a0f9cb5c3d0e4d2f00
josbra-attensi <josbra@attensi.com>
2024-12-16 14:54:48 +0100

    beep: boop

---
+++ src/lib/ScrollItem.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		stepNumber: number;
+		showStepBackgrounds: boolean;
+		bgColor: string;
+		children: Snippet<[]>;
+	}
+	const { stepNumber, showStepBackgrounds, children }: Props = $props();
+</script>
+
+<div
+	class="flex min-h-dvh w-full snap-mandatory snap-start snap-always flex-col items-center justify-center gap-2 py-2"
+	class:{bgColor}={showStepBackgrounds}
+	id="step-{stepNumber}"
+>
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
+	import ScrollItem from '$lib/ScrollItem.svelte';
+	import { tick, type Snippet } from 'svelte';
 	import { fly } from 'svelte/transition';
 
 	let scrollingContainer = $state<HTMLDivElement | undefined>();
 	let showStepBackgrounds = $state(true);
 	let showToast = $state(false);
 	let gameStarted = $state(false);
+	let stepRefs = $state<HTMLElement[]>([]);
 
 	let visibleSteps = $state<Snippet[]>([start]);
 	let steps: Snippet[] = [start, information, paragraph, trueFalse, multipleChoice, summary];
@@ -16,8 +18,18 @@
 		triggerToast();
 	}
 
-	function showNextStep(i: number) {
+	async function scrollToStep(i: number) {
+		await tick();
+		const step = stepRefs[i];
+		if (step) {
+			step.scrollIntoView({ behavior: 'smooth' });
+		}
+	}
+
+	async function showNextStep(i: number) {
 		visibleSteps = [...visibleSteps, steps[i + 1]];
+		await tick();
+		scrollToStep(i + 1);
 	}
 
 	function triggerToast() {
@@ -33,18 +45,17 @@
 				scrollingContainer.scrollTop + scrollingContainer.clientHeight >=
 				scrollingContainer.scrollHeight;
 
-			if (atBottom && gameStarted) {
+			let isLastStep = visibleSteps.length === steps.length;
+
+			if (isLastStep) return;
+
+			if (atBottom && gameStarted && visibleSteps.length < steps.length - 1) {
 				triggerToast();
-				setTimeout(() => showNextStep(visibleSteps.length), 500);
+				setTimeout(() => showNextStep(visibleSteps.length), 1000);
 			}
 		}
 	}
 </script>
-
-<!-- <button -->
-<!-- 	class="absolute bottom-0 right-0 rounded-lg bg-white p-2 text-xs shadow shadow-black" -->
-<!-- 	onclick={() => (showStepBackgrounds = !showStepBackgrounds)}>BG</button -->
-<!-- > -->
 
 {#if showToast}
 	<div
@@ -62,145 +73,116 @@
 	bind:this={scrollingContainer}
 	{onscroll}
 >
+	<div class="sticky top-0 h-[100px]">Testing</div>
 	{#each visibleSteps as stepSnippet, i (i)}
-		{@render stepSnippet?.()}
+		<ScrollItem stepNumber={i} {showStepBackgrounds} bgColor="bg-red-200 bind:this={stepRefs[i]}">
+			{@render stepSnippet?.()}
+		</ScrollItem>
 	{/each}
 </div>
 
 {#snippet start()}
-	<div
-		class="flex min-h-dvh w-full snap-mandatory snap-start flex-col items-center justify-center gap-2 py-2"
-		class:bg-red-200={showStepBackgrounds}
-		id="start"
-	>
-		<div class="mt-2 w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
-			<h2 class="text-xl font-black">Start</h2>
+	<div class="mt-2 w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
+		<h2 class="text-xl font-black">Start</h2>
 
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
-				Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo
-				eius repellat, consectetur, distinctio deserunt!
-			</p>
-		</div>
-		<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
-				Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo
-				eius repellat, consectetur, distinctio deserunt!
-			</p>
-		</div>
-		<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
-				Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo
-				eius repellat, consectetur, distinctio deserunt!
-			</p>
-		</div>
-		<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
-				Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo
-				eius repellat, consectetur, distinctio deserunt!
-			</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
+			Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo eius
+			repellat, consectetur, distinctio deserunt!
+		</p>
+	</div>
+	<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
+			Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo eius
+			repellat, consectetur, distinctio deserunt!
+		</p>
+	</div>
+	<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
+			Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo eius
+			repellat, consectetur, distinctio deserunt!
+		</p>
+	</div>
+	<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
+			Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo eius
+			repellat, consectetur, distinctio deserunt!
+		</p>
 
-			<button class="rounded-lg bg-blue-500 p-2 text-white" onclick={startGame}>Start Game</button>
-		</div>
+		<button class="rounded-lg bg-blue-500 p-2 text-white" onclick={startGame}>Start Game</button>
 	</div>
 {/snippet}
 
 {#snippet information()}
-	<div
-		class="mt-2 flex min-h-dvh w-full snap-mandatory snap-start flex-col items-center justify-center"
-		class:bg-blue-200={showStepBackgrounds}
-	>
-		<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
-			<h2 class="text-xl font-black">Information</h2>
+	<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
+		<h2 class="text-xl font-black">Information</h2>
 
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
-				Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo
-				eius repellat, consectetur, distinctio deserunt!
-			</p>
-		</div>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
+			Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo eius
+			repellat, consectetur, distinctio deserunt!
+		</p>
 	</div>
 {/snippet}
 
 {#snippet paragraph()}
-	<div
-		class="mt-2 flex min-h-dvh w-full snap-mandatory snap-start flex-col items-center justify-center"
-		class:bg-green-200={showStepBackgrounds}
-	>
-		<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
-			<h2 class="text-xl font-black">Paragraph</h2>
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
-				Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo
-				eius repellat, consectetur, distinctio deserunt!
-			</p>
-		</div>
+	<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
+		<h2 class="text-xl font-black">Paragraph</h2>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
+			Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo eius
+			repellat, consectetur, distinctio deserunt!
+		</p>
 	</div>
 {/snippet}
 
 {#snippet trueFalse()}
-	<div
-		class="mt-2 flex min-h-dvh w-full snap-mandatory snap-start flex-col items-center justify-center"
-		class:bg-yellow-200={showStepBackgrounds}
-	>
-		<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
-			<h2 class="text-xl font-black">True False</h2>
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
-				Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo
-				eius repellat, consectetur, distinctio deserunt!
-			</p>
-		</div>
+	<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
+		<h2 class="text-xl font-black">True False</h2>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
+			Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo eius
+			repellat, consectetur, distinctio deserunt!
+		</p>
 	</div>
 {/snippet}
 
 {#snippet multipleChoice()}
-	<div
-		class="mt-2 flex min-h-dvh w-full snap-mandatory snap-start flex-col items-center justify-center gap-4"
-		class:bg-purple-200={showStepBackgrounds}
-	>
-		<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
-			<h2 class="text-xl font-black">Multiple Choice</h2>
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
-				Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo
-				eius repellat, consectetur, distinctio deserunt!
-			</p>
-		</div>
+	<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
+		<h2 class="text-xl font-black">Multiple Choice</h2>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
+			Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo eius
+			repellat, consectetur, distinctio deserunt!
+		</p>
+	</div>
 
-		<div class="flex w-5/6 flex-col gap-4">
-			<div class="rounded-sm bg-white p-4">Option 1</div>
-			<div class="rounded-sm bg-white p-4">Option 2</div>
-			<div class="rounded-sm bg-white p-4">Option 3</div>
-			<div class="rounded-sm bg-white p-4">Option 4</div>
-			<div class="rounded-sm bg-white p-4">Option 5</div>
-			<div class="rounded-sm bg-white p-4">Option 6</div>
-			<div class="rounded-sm bg-white p-4">Option 7</div>
-			<div class="rounded-sm bg-white p-4">Option 8</div>
-			<div class="rounded-sm bg-white p-4">Option 9</div>
-			<div class="rounded-sm bg-white p-4">Option 10</div>
-		</div>
+	<div class="flex w-5/6 flex-col gap-4">
+		<div class="rounded-sm bg-white p-4">Option 1</div>
+		<div class="rounded-sm bg-white p-4">Option 2</div>
+		<div class="rounded-sm bg-white p-4">Option 3</div>
+		<div class="rounded-sm bg-white p-4">Option 4</div>
+		<div class="rounded-sm bg-white p-4">Option 5</div>
+		<div class="rounded-sm bg-white p-4">Option 6</div>
+		<div class="rounded-sm bg-white p-4">Option 7</div>
+		<div class="rounded-sm bg-white p-4">Option 8</div>
+		<div class="rounded-sm bg-white p-4">Option 9</div>
+		<div class="rounded-sm bg-white p-4">Option 10</div>
 	</div>
 {/snippet}
 
 {#snippet summary()}
-	<div
-		class="mt-2 flex min-h-dvh w-full snap-mandatory snap-start flex-col items-center justify-center"
-		class:bg-gray-200={showStepBackgrounds}
-	>
-		<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
-			<h2 class="text-xl font-black">Summary</h2>
+	<div class="w-5/6 rounded-md border border-gray-700 bg-white p-5 md:w-1/2">
+		<h2 class="text-xl font-black">Summary</h2>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
+			Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo eius
+			repellat, consectetur, distinctio deserunt!
+		</p>
 
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque laudantium explicabo nam!
-				Consequatur vero cumque ratione optio? Provident quo quia totam tempora iste dolorem nemo
-				eius repellat, consectetur, distinctio deserunt!
-			</p>
-
-			<a class="cursor-pointer text-blue-500 hover:font-bold" href="#start">Back to start</a>
-		</div>
+		<a class="cursor-pointer text-blue-500 hover:font-bold" href="#step-0">Back to start</a>
 	</div>
 {/snippet}
