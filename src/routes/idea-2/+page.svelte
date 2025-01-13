1931240b79dc4243a8910e2f72c655787a818f6c
josbra-attensi <josbra@attensi.com>
2025-01-13 15:24:21 +0100

    chore: trim fat - CSS vh calculations may not be needed with this approach

--- src/routes/idea-2/+page.svelte
+++ src/routes/idea-2/+page.svelte
@@ -49,14 +49,14 @@
 <div
 	class="grid-layout max-h-screen min-h-screen w-full bg-gradient-to-t from-indigo-400 to-indigo-200 bg-fixed"
 >
-	<div id="header" class="fast-header h-[var(--header-height)] w-full bg-pink-200">
+	<div id="header" class="fast-header w-full bg-pink-200">
 		<div class="h-12 w-fit bg-green-200">Header</div>
 	</div>
 	{#key currentStep}
 		<div
 			in:fly={{ y: '100%', easing: cubicOut, delay: 500 }}
 			out:fly={{ y: '-100%', easing: expoIn }}
-			class="fast-content flex h-[var(--step-height)] w-[100vw] snap-y snap-mandatory flex-col overflow-y-auto"
+			class="fast-content flex w-[100vw] snap-y snap-mandatory flex-col overflow-y-auto"
 		>
 			{#each currentStepGroup.steps as StepComponent}
 				<StepComponent onStepComplete={StepComponent === Summary ? goToStartStep : goToNextStep} />
@@ -67,7 +67,7 @@
 			{/if}
 		</div>
 	{/key}
-	<div id="footer" class="fast-footer h-[var(--footer-height)] w-full bg-pink-200">
+	<div id="footer" class="fast-footer w-full bg-pink-200">
 		<div class="h-12 w-fit bg-green-200">Footer</div>
 	</div>
 </div>
